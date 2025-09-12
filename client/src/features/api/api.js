81ee1aab36817333293857dfe872f15f2ca76d5f
Bibek Kunwar <bibekkunwar238@gmail.com>
2025-09-12 22:28:57 +0545

    Update api

--- client/src/features/api/api.js
+++ client/src/features/api/api.js
@@ -1,10 +1,14 @@
 // src/utils/api.js
 import axios from "axios";
 
+console.log("🚀 VITE_API_URL at runtime:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   withCredentials: true,
 });
+
+console.log("✅ Axios Base URL:", API.defaults.baseURL);
 
 // Get recommended courses
 export const getRecommendedCourses = async (userId) => {
